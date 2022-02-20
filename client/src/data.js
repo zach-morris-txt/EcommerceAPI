0cfc03e7e8aa4c22bc8a8a4e704697a0f3c8287a
zach-morris-txt <zach-morris@lambdastudents.com>
2022-02-20 18:08:08 -0500

    Refactor; Category Data

--- client/src/data.js
+++ client/src/data.js
@@ -22,58 +22,41 @@ export const sliderItems = [
     },
   ];
 
-  export const categories = [
+export const categories = [
     {
       id: 1,
-      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "SHIRT STYLE!",
-      cat:"women"
+      img: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F2016%2F05%2FJacket-Free-Download-PNG.png&f=1&nofb=1",
+      title: "Striped Leather Jacket",
+      cat:"Jackets"
     },
     {
       id: 2,
-      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "LOUNGEWEAR LOVE",
-      cat:"coat"
+      img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages-na.ssl-images-amazon.com%2Fimages%2FI%2F91wK6mdDwlL._SL1500_.jpg&f=1&nofb=1",
+      title: "Dual Sport Motorcycle Pants",
+      cat:"Pants"
     },
     {
       id: 3,
-      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      title: "LIGHT JACKETS",
-      cat:"jeans"
-    },
-  ];
-
-  export const popularProducts = [
-    {
-      id:1,
-      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+      img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi5.walmartimages.com%2Fasr%2F0c8cf450-0a87-4752-97a1-8db728e45eed_1.f10c81fac0e107e829afca9abf40ec58.jpeg&f=1&nofb=1",
+      title: "Fingerless Leather Gloves",
+      cat:"Gloves"
     },
     {
-      id:2,
-      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+      id: 1,
+      img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi5.walmartimages.com%2Fasr%2F28862137-4c65-4205-bc66-a44936d118f1_1.8557f8c41259e685dcf88d7385c360ed.jpeg&f=1&nofb=1",
+      title: "Pink Skull Helmet",
+      cat:"Helmets"
     },
     {
-      id:3,
-      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+      id: 2,
+      img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faltimategear.com%2Fwp-content%2Fuploads%2F2014%2F01%2FRed-Msx1-Copy.jpg&f=1&nofb=1",
+      title: "White Flame Helmet",
+      cat:"Helmets"
     },
     {
-      id:4,
-      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+      id: 3,
+      img: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fbritishmotorcyclegear.com%2Fmedia%2Fcatalog%2Fproduct%2Fcache%2F1%2Fimage%2F9df78eab33525d08d6e5fb8d27136e95%2Fi%2Fn%2Findian-brown-laces-red.jpg&f=1&nofb=1",
+      title: "Waterproof Motorcycle Boots",
+      cat:"Shoes"
     },
-    {
-      id:5,
-      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
-    },
-    {
-      id:6,
-      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
-    },
-    {
-      id:7,
-      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
-    },
-    {
-      id:8,
-      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
-    },
-  ]
+];
