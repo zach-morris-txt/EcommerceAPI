9d013131cca993c23b821189e551f1e5608d4a5f
zach-morris-txt <zach-morris@lambdastudents.com>
2022-02-21 22:36:32 -0500

    Heroku Deploy 4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,16 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import Success from "./pages/Success";
+
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
-import Success from "./pages/Success";
 import { useSelector } from "react-redux";
+
 
 const App = () => {
   const user = useSelector((state) => state.user.currentUser);
